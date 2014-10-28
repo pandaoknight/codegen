d3fb8210111ecde85a1f265797790217318bfa8e
PC <pandaoknight@users.noreply.github.com>
2014-10-28 15:47:32 +0800

    [调整]准备Merge到codegen中

--- codegen/__init__.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: utf-8 -*-
-from namegen import namegen
-
-
-def gen_php_router(name):
-    n = namegen(name)
-    return "'/%s' => '%s'," % (name, n.controller_name())
--- tryit.sh
+++
@@ -1,3 +0,0 @@
-#DIR=$(dirname "${0}")
-#sh $DIR"/dir/tryit.sh"
-python main.py -fthefile.txt -s xyz a1 a2 a3
